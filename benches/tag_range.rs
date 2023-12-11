21662c0c243f17a151f03a08eeb68a721879b559
AlexisGado <ipod.alexisgado@gmail.com>
2023-12-11 01:52:52 -0500

    add benchmarks (still wip)

---
+++ benches/tag_range.rs
@@ -0,0 +1,25 @@
+mod common;
+use criterion::{criterion_group, criterion_main, Criterion};
+use order_maintenance::tag_range::Priority;
+
+macro_rules! create_bench_functions {
+    () => {};
+    ($bench_name:ident($c:ident $(,$arg:expr)*); $($toks:tt)*) => {
+        $c.bench_function(stringify!(tag-range_$bench_name$(_$arg)*), |b| {
+            common::benches::$bench_name::<Priority>(b, $($arg,)*);
+        });
+        create_bench_functions!{$($toks)*}
+    };
+}
+pub fn tag_range_benchmark(c: &mut Criterion) {
+    create_bench_functions!(
+        insert_random(c, 10);
+        insert_random(c, 1000);
+        insert_random(c, 100_000);
+        comparisons(c);
+        sort(c);
+    );
+}
+
+criterion_group!(benches, tag_range_benchmark);
+criterion_main!(benches);
